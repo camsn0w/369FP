24473020a289601c898245edefb5288c5f377a58
camsn0w <camhconnell@gmail.com>
2020-05-04 15:42:22 -0600

    Started part B

--- T1.py
+++ T1.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 
+
 def relPrime(num):
     if num <= 2:
         if num == 1:
@@ -17,19 +18,39 @@ def relPrime(num):
 
 def printRelPrimeTable(inpt):
     for x in inpt:
-        print(x, "Sum:", inpt[x][1])
+        print(x, "Coprimes:", inpt[x][0])
+        print("Sum:", inpt[x][1])
+
 
 def relPrimeTable(inpt):
     sumDict = {}
     for x in inpt:
         sumDict.setdefault(x, [relPrime(x), sum(relPrime(x))])
-    printRelPrimeTable(sumDict)
+    return sumDict
+    # printRelPrimeTable(sumDict)
+
+
+def c1(inpt):
+    for x in inpt:
+        print("n =", x)
+        print("Sum =", inpt[x][1])
+        val = inpt[x][1] % int(x)
+        print("Sum % n =", val)
+
+
+# Builds matrix shown in B for a given int
+def genPnMatrix(num):
+    inpt = relPrime(num)
+    newlist = [inpt[x:] + inpt[:x] for x in range(0, len(inpt))]
+    print(np.matrix(newlist))
+    return newlist
 
 
 def main():
-    listoNums = [i for i in range(2, 16, 1)]
-    print(relPrimeTable(listoNums))
-    #relPrime(listoNums)
+    listoNums = [i for i in range(2, 30, 1)]
+    genPnMatrix(9)
+    # print(relPrimeTable(listoNums))
+    # c1(relPrimeTable(listoNums))
 
 
 if __name__ == '__main__':
