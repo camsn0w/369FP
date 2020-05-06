0402f8a904890939220f8835c8422e9f8ee11d0f
camsn0w <camhconnell@gmail.com>
2020-05-06 15:07:15 -0600

    Printed out matrix for n={0,26}

--- T2.py
+++ T2.py
@@ -6,14 +6,13 @@ import T1 as T1
 def EulerPhi(num):
     return len(T1.relPrime(num))
 
-
 def main():
-    listoNums = [i for i in range(2, 26, 1)]
-    print("ϕ(n) for n = 2, 3,..., 25")
-    print([EulerPhi(i) for i in range(2, 26, 1)])
-    #print(EulerPhi(6))
+    print("ϕ(n) for n =", [i for i in range(2, 27, 1)])
+    ep = [EulerPhi(i) for i in range(2, 27, 1)]
+    print(ep)
+    epMatrix = np.matrix([ep[r*5:(r+1)*5] for r in range(0,5)])
+    print(epMatrix)
 
 
 if __name__ == '__main__':
     main()
-
