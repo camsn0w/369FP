f804b5e3a5ff157ce021f76cf095c093053f6a9a
camsn0w <camhconnell@gmail.com>
2020-05-12 15:52:29 -0600

    Added comments to each method

--- T1.py
+++ T1.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 
 
-def relPrime(num):
+def relPrime(num):  # Finds the numbers that are relatively prime to num
     if num <= 2:
         if num == 1:
             return [0]
@@ -16,14 +16,15 @@ def relPrime(num):
     return result
 
 
-def printRelPrimeTable(inpt):
+def printRelPrimeTable(inpt):  # Prints a neat table that contains and validates the conjectures for n > 2, note that
+    # for numbers larger than 15, due to floating point accuracy loss, may not validate conjecture
     inpt = relPrimeTable(inpt)
     for x in inpt:
         print("n =", x, " S\u2099=", end="{"), print(*inpt[x][0], sep=", ", end="}\n"),
-        print("S\u2099=", inpt[x][1])
+        print("Sum S\u2099=", inpt[x][1])
         print('Matrix: \n', np.matrix(genPnMatrix(x)))
         print("Determinant: ", round(np.linalg.det(genPnMatrix(x))))
-        print("Determinant % {}: ".format(x), round(np.linalg.det(genPnMatrix(x))%4))
+        print("Determinant % {}: ".format(x), round(np.linalg.det(genPnMatrix(x))) % x)
         print("{{x\u2081,x\u2082} ⊂ S\u2099| x\u2081+x\u2082 = n, |S\u2099(1,x\u2081)| = |S\u2099(x\u2082,n-1)|}")
         for i in range(int(len(inpt[x][0]) / 2)):
             x1 = inpt[x][0][int(i)]
@@ -32,14 +33,14 @@ def printRelPrimeTable(inpt):
         print("_" * 60)
 
 
-def relPrimeTable(inpt):
+def relPrimeTable(inpt):  # Helper method for the table method that finds the sum of the relPrimes and maps that to n
     sumDict = {}
     for x in inpt:
         sumDict.setdefault(x, [relPrime(x), sum(relPrime(x))])
     return sumDict
 
 
-def c1(inpt):
+def c1(inpt):  # No longer in use method that was used to originally test the conjecture
     for x in inpt:
         print("n =", x)
         print("Sum =", inpt[x][1])
@@ -54,7 +55,7 @@ def genPnMatrix(num):
     return newlist
 
 
-def bDetWork(num):
+def bDetWork(num):  # No longer in use method that tested determinant related conjectures
     inpt = genPnMatrix(num)
     det1 = np.linalg.det(inpt)
     det2 = np.linalg.det(inpt) % num
@@ -63,7 +64,7 @@ def bDetWork(num):
 
 
 def main():
-    listoNums = [i for i in range(2, 26, 1)]
+    listoNums = [i for i in range(2, 16, 1)]
     printRelPrimeTable(listoNums)
 
 
